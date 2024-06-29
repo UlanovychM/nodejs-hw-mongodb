daa1171fa26696e2620ecdb9d229e17e70fc54e2
Mykhailo <m.ulanovych@gmail.com>
2024-06-29 14:56:16 +0300

    'update'

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -2,14 +2,14 @@ import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { registerUserSchema } from '../validation/auth.js';
 import { registerUserController } from '../controllers/auth.js';
-import { validateBody } from '../middlewares/validateBody.js';
+import { validateBody } from '../middleware/validateBody.js';
 import { loginUserSchema } from '../validation/auth.js';
 import {
   loginUserController,
   logoutUserController,
   refreshUserSessionController,
 } from '../controllers/auth.js';
-import { authenticate } from '../middlewares/authenticate.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 const router = Router();
 
